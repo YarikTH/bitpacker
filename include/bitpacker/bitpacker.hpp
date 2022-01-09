eb726e0a58547cc74b09bc8ab3bcaa28cc6ceec1
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2022-01-09 11:39:02 +0300

    Add bit_width fallback

--- include/bitpacker/bitpacker.hpp
+++ include/bitpacker/bitpacker.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+#if !defined( BITPACKER_USE_STD_BIT )
+#    define BITPACKER_USE_STD_BIT __has_include( <bit> )
+#endif
+
+#if BITPACKER_USE_STD_BIT
+#    include <bit>
+#endif
+
 #define BITPACKER_VERSION_MAJOR 0
 #define BITPACKER_VERSION_MINOR 1
 #define BITPACKER_VERSION_PATCH 0
@@ -9,4 +17,27 @@
     ( BITPACKER_VERSION_MAJOR * 10000 + BITPACKER_VERSION_MINOR * 100 + BITPACKER_VERSION_PATCH )
 
 namespace bitpacker
-{}
+{
+
+#if BITPACKER_USE_STD_BIT
+
+using std::bit_width;
+
+#else
+
+template <class T, std::enable_if_t<std::is_unsigned_v<T>, int> = 0>
+[[nodiscard]] constexpr T bit_width( const T value ) noexcept
+{
+    T result = 0u;
+    T temp = value;
+    while( temp != 0u )
+    {
+        ++result;
+        temp >>= static_cast<T>( 1u );
+    }
+    return result;
+}
+
+#endif
+
+} // namespace bitpacker
--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,2 +1,21 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
+
+#include "bitpacker/bitpacker.hpp"
+
+static_assert( bitpacker::bit_width( 3u ) );
+static_assert( bitpacker::bit_width( 4u ) );
+static_assert( bitpacker::bit_width( std::numeric_limits<unsigned char>::max() )
+               == std::numeric_limits<unsigned char>::digits );
+static_assert( bitpacker::bit_width( std::numeric_limits<std::uint64_t>::max() )
+               == std::numeric_limits<std::uint64_t>::digits );
+
+TEST_CASE( "bit_width" )
+{
+    CHECK( bitpacker::bit_width( 3u ) == 2 );
+    CHECK( bitpacker::bit_width( 4u ) == 3 );
+    CHECK( bitpacker::bit_width( std::numeric_limits<unsigned char>::max() )
+           == std::numeric_limits<unsigned char>::digits );
+    CHECK( bitpacker::bit_width( std::numeric_limits<std::uint64_t>::max() )
+           == std::numeric_limits<std::uint64_t>::digits );
+}
